cf48f533fda128a186e4b6259538a13feb719fae
benscott <ben@benscott.co.uk>
2017-02-07 21:15:28 +0000

    Fix load schema command

--- lodb/commands.py
+++ lodb/commands.py
@@ -2,9 +2,17 @@
 """Click commands.
 
 """
-
+import os
+import json
 import click
 import requests
+from flask.helpers import get_debug_flag
+from xsdtojson import xsd_to_json_schema
+
+# FIXME: This should get config from application context
+from lodb.config import DevelopmentConfig, ProductionConfig
+CONFIG = DevelopmentConfig if get_debug_flag() else ProductionConfig
+
 
 try:
     from urllib.parse import urlparse
@@ -14,14 +22,22 @@ except ImportError:
 
 @click.command()
 @click.argument('uri')
-def install_schema(uri):
+def load_schema(uri):
     """Import schema - either JSON schema or XSD to be converted to JSON schema """
+    file_name, file_ext = os.path.splitext(os.path.basename(uri))
+    if file_ext != '.xsd':
+        raise Exception('Only XSD files can be loaded at this point')
     parsed_url = urlparse(uri)
     # Is this a remote file
     if parsed_url.netloc:
         r = requests.get(uri)
+        r.raise_for_status()
+        json_schema = xsd_to_json_schema(r.content)
     else:
-        print('LOCAL')
-    # value = click.prompt('Please enter a valid integer', type=int, default=2)
-    # print(value)
-    print('hey')
+        json_schema = xsd_to_json_schema(uri)
+
+    schema_file = os.path.join(CONFIG.SCHEMA_DIR, '{file_name}.schema.json'.format(file_name=file_name))
+    # Write schema to file
+    with open(schema_file, 'w') as f:
+        f.write(json_schema + '\n')
+
